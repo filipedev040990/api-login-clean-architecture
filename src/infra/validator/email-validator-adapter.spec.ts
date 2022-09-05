6b5c68f0bc0f3cd3d9fc399d18103701500a165c
Filipe Siqueira <filipedev040990@gmail.com>
2022-09-05 12:37:15 -0300

    feat: ensure return true if validator return true

--- src/infra/validator/email-validator-adapter.spec.ts
+++ src/infra/validator/email-validator-adapter.spec.ts
@@ -20,4 +20,10 @@ describe('EmailValidatorAdapter', () => {
     await sut.isValid('anyEmail@email.com')
     expect(isValidSpy).toHaveBeenCalledWith('anyEmail@email.com')
   })
+
+  test('should return true if validator return true', async () => {
+    const sut = makeSut()
+    const isValid = await sut.isValid('anyEmail@email.com')
+    expect(isValid).toBe(true)
+  })
 })
